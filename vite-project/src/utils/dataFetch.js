811378e3f429ee12b33298e871169599ae8fbaad
Arlette <arportillaca@uide.edu.ec>
2023-07-19 17:12:25 -0500

    demo

---
+++ vite-project/src/utils/dataFetch.js
@@ -0,0 +1,7 @@
+const dataFetch =async () =>{
+    const URL = "https://rickandmortyapi.com/api/character"
+    const response = await fetch(URL).then(response => response.json());
+    return response;
+}
+
+export default dataFetch
